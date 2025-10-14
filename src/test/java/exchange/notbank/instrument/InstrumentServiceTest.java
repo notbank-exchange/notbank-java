46de3105940fef347b7e54dd681d5687c25de22f
T. Ismael Verdugo <ismael@cryptomkt.com>
2025-10-14 04:33:32 -0300

    test: instrument service

--- src/test/java/exchange/notbank/instrument/InstrumentServiceTest.java
+++ src/test/java/exchange/notbank/instrument/InstrumentServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,43 +12,55 @@ import org.junit.jupiter.api.Test;
 import exchange.notbank.NotbankClient;
 import exchange.notbank.TestHelper;
 import exchange.notbank.core.CompletableFutureAdapter;
+import exchange.notbank.instrument.constants.InstrumentState;
 import exchange.notbank.instrument.paramBuilders.GetInstrumentParamBuilder;
+import exchange.notbank.instrument.paramBuilders.GetInstrumentVerificationLevelConfigParamBuilder;
 import exchange.notbank.instrument.paramBuilders.GetInstrumentsParamBuilder;
 
 public class InstrumentServiceTest {
-  private static InstrumentService service;
+  private static NotbankClient client;
 
   @BeforeAll
   public static void beforeAll() {
-    service = NotbankClient.Factory.createRestClient(TestHelper.HOST).getInstrumentService();
+    client = TestHelper.newRestClient();
   }
 
   @Test
   public void getInstruments() {
-    var futureResponse = service.getInstruments(new GetInstrumentsParamBuilder());
+    var futureResponse = client.getInstrumentService().getInstruments(new GetInstrumentsParamBuilder().instrumentState(InstrumentState.BOTH));
     TestHelper.checkNoError(futureResponse);
   }
 
   @Test
   public void getInstrument() {
-    var futureResponse = service.getInstrument(new GetInstrumentParamBuilder(1));
+    var futureResponse = client.getInstrumentService().getInstrument(new GetInstrumentParamBuilder(1));
     TestHelper.checkNoError(futureResponse);
   }
+
+  @Test
+  public void getInstrumentVerificationLevelConfig() throws InterruptedException, ExecutionException {
+    var credentials = TestHelper.getUserCredentials();
+    client.authenticate(credentials.userId, credentials.apiPublicKey, credentials.apiSecretKey).get();
+    var futureResponse = client.getInstrumentService().getInstrumentVerificationLevelConfig(new GetInstrumentVerificationLevelConfigParamBuilder(credentials.accountId));
+    TestHelper.checkNoError(futureResponse);
+  }
+
+  
 
   @Test
   public void getInstrumentId() {
     var btcUsdtInstrumentId = 154;
     var btcUsdtSymbol = "BTCUSDT";
-    var firstCallResponse = service.getInstrumentId(btcUsdtSymbol);
+    var firstCallResponse = client.getInstrumentService().getInstrumentId(btcUsdtSymbol);
     checkInstrumentIdResponse(btcUsdtInstrumentId, firstCallResponse);
-    var secondCallResponse = service.getInstrumentId(btcUsdtSymbol);
+    var secondCallResponse = client.getInstrumentService().getInstrumentId(btcUsdtSymbol);
     checkInstrumentIdResponse(btcUsdtInstrumentId, secondCallResponse);
   }
 
   @Test
   public void getInstrumentIdFailsOnInvalidSymbol() {
     var invalidSymbol = "ABCABC";
-    var futureResponse = service.getInstrumentId(invalidSymbol);
+    var futureResponse = client.getInstrumentService().getInstrumentId(invalidSymbol);
     var response = CompletableFutureAdapter.getToEither(futureResponse);
     assertTrue(response.isLeft());
   }
