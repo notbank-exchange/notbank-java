acf4fa36215fd43efe246c994563069b7ef0906b
T. Ismael Verdugo <ismael@cryptomkt.com>
2025-10-17 10:28:17 -0300

    fix: renames enum class

--- src/main/java/exchange/notbank/trading/responses/EnumClass.java
+++ src/main/java/exchange/notbank/trading/responses/EnumClass.java
@@ -6,20 +6,20 @@ import com.squareup.moshi.Json;
 
 public class EnumClass {
   @Json(name = "Class")
-  public final String enumClass;
+  public final String parentClass;
   @Json(name = "Property")
   public final String property;
   @Json(name = "Enums")
   public final List<EnumValue> enums;
 
   public EnumClass(String enumClass, String property, List<EnumValue> enums) {
-    this.enumClass = enumClass;
+    this.parentClass = enumClass;
     this.property = property;
     this.enums = enums;
   }
 
   @Override
   public String toString() {
-    return "EnumClass [enumClass=" + enumClass + ", property=" + property + ", enums=" + enums + "]";
+    return "EnumClass [parentClass=" + parentClass + ", property=" + property + ", enums=" + enums + "]";
   }
 }
